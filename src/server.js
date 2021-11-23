5f9f3e1d6da8fcb088c978629212fe4b18032dbf
kjm541006 <kjm541113@gmail.com>
2021-11-23 20:50:28 +0900

    Initial Commit

--- src/server.js
+++ src/server.js
@@ -1,21 +1,25 @@
 import express from "express";
+import morgan from "morgan";
 
-const PORT = 4000;
+const PORT = 4000; //관습, 높은숫자는 비어있을 확률 높음
 
 const app = express();
+const logger = morgan("dev");
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+//last controller에는 관습적으로 next를 쓰지않음
+const home = (req, res) => {
+  return res.send("hello"); //return을 만나면 종료
 };
 
-const handleHome = (req, res) => {
-  return res.send("hello");
+const login = (req, res) => {
+  return res.send("Welcome to the private");
 };
 
-app.use(gossipMiddleware);
-//get은 request를 처리함.
-app.get("/", handleHome);
+//use는 모든곳에서 적용
+app.use(logger);
+//get은 request를 처리함
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
