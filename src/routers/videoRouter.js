f024b6338a5c422317614ec9c1f73a8ff78edbfb
kjm541006 <kjm541113@gmail.com>
2022-01-25 01:37:42 +0900

    delete video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,7 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,6 +13,8 @@ const videoRouter = express.Router();
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.get("/:id([0-9a-f]{24})/edit", getEdit);
 videoRouter.post("/:id([0-9a-f]{24})/edit", postEdit);
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
+
 videoRouter.get("/upload", getUpload);
 videoRouter.post("/upload", postUpload);
 export default videoRouter;
