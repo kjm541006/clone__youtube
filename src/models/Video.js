c1fff3a43efe001e37661048f672845f1f8da96a
kjm541006 <kjm541113@gmail.com>
2022-01-24 23:33:55 +0900

    Video Model

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,21 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
+  title: { type: String, required: true, trim: true, maxlength: 80 },
+  description: { type: String, required: true, trim: true, minlength: 20 },
+  createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String }],
   meta: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
   },
+});
+
+videoSchema.pre("save", async function () {
+  console.log("We are about to save", this);
+  this.hashtags = this.hashtags[0]
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
